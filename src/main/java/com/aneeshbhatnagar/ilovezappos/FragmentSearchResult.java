59d7e264a664d99db3406960b6869c31f467d841
Aneesh Bhatnagar <aneesh.bhatnagar93@gmail.com>
2017-02-09 16:31:45 -0500

    Query Server with Android Volley for Results

--- src/main/java/com/aneeshbhatnagar/ilovezappos/FragmentSearchResult.java
+++ src/main/java/com/aneeshbhatnagar/ilovezappos/FragmentSearchResult.java
@@ -1,17 +1,30 @@
 package com.aneeshbhatnagar.ilovezappos;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.JsonReader;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
 
 /**
  * Created by aneesh on 2/9/17.
  */
 
 public class FragmentSearchResult extends Fragment {
-
+    String queryText;
+    View rootView;
     public FragmentSearchResult() {
         // Required empty public constructor
     }
@@ -24,8 +37,30 @@ public class FragmentSearchResult extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_search_result, container, false);
+        queryText = getArguments().getString("query");
+        rootView = inflater.inflate(R.layout.fragment_search_result, container, false);
         // Inflate the layout for this fragment
         return rootView;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        RequestQueue requestQueue = Volley.newRequestQueue(getContext());
+        String url = "https://api.zappos.com/Search?key=b743e26728e16b81da139182bb2094357c31d331&term=" + queryText;
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d("Response",response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+        requestQueue.add(stringRequest);
     }
 }
