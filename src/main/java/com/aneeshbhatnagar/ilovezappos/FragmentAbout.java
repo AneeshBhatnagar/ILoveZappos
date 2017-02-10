d91fae34d499d561da10ec03bc0655cc4820d1f6
Aneesh Bhatnagar <aneesh.bhatnagar93@gmail.com>
2017-02-10 16:27:29 -0500

    Added About Me text

--- src/main/java/com/aneeshbhatnagar/ilovezappos/FragmentAbout.java
+++ src/main/java/com/aneeshbhatnagar/ilovezappos/FragmentAbout.java
@@ -1,16 +1,22 @@
 package com.aneeshbhatnagar.ilovezappos;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by aneesh on 2/9/17.
  */
 
-public class FragmentAbout extends Fragment{
+public class FragmentAbout extends Fragment {
+    TextView textView;
+    View rootView;
 
     public FragmentAbout() {
         // Required empty public constructor
@@ -25,10 +31,24 @@ public class FragmentAbout extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_about, container, false);
+        rootView = inflater.inflate(R.layout.fragment_about, container, false);
 
 
         // Inflate the layout for this fragment
         return rootView;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        textView = (TextView) rootView.findViewById(R.id.about_content);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse("http://www.aneeshbhatnagar.com"));
+                startActivity(i);
+            }
+        });
     }
 }
